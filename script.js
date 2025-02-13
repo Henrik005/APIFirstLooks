baff093991cbb17791e3ca910b17ffe3b090f9ef
Henrik005 <henrik.mathias.brathen@gmail.com>
2025-02-13 10:41:59 +0100

    Update

--- script.js
+++ script.js
@@ -7,34 +7,38 @@ let fetchData = [];
 
 
 function getDataWithAxios(){ 
-    return axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(response =>{
+     axios.get('https://jsonplaceholder.typicode.com/users')
+     .then(response =>{
         console.log('Data Retrieved Succesfully')
         console.log(response.data);
-       
-        displayData(axiosData);
+        axiosData = response.data;
     })
     .catch(error => {
       console.error('There has been an error:', error);
     })
   
 }
-
+//Here is both of my data aquisition functions, one uses Fetch and the other Axios, but they both ultimatly do the same thing.
+//Once they have the data from the API that i want, i console log it and then add it to their own respective arrays near the top of the file.
+//Wich i then use in my printData() function to display it in the HTML document by looping trough the data with a forEach().
+//The primary difference between Fetch and Axios is that Axios is more automated, it does things like converting the response to json-
+//-and handling http errors on its own which Fetch doesn't do, hence why i have if(response.ok) and return response.json inside of the Fetch function-
+//-but not the Axios function. However Fetch has one up on Axios in the sense that it is built in, so, you don't need external libraries to use it.
+//So, you don't have to import it or anything you can use it right off the rip.
  function getDataWithFetch(){
-    return fetch('https://jsonplaceholder.typicode.com/posts')
+     fetch('https://jsonplaceholder.typicode.com/posts')
 .   then(response =>{
     if(response.ok){
         console.log('Data Retrieved Succesfully')
     }
     return response.json();
     })
-    
-    .then (data => console.log(data))
+    .then(response =>{fetchData = response
+        console.log(response)
+    })
     .catch(error => console.log('There has been an error:', error));
-
+        
 }
-
-
 
 function updateView(){
     app.innerHTML=/*HTML*/`
@@ -44,25 +48,33 @@ function updateView(){
     `
 }
 
-function displayData(data) {
-    if (data == false || data.length === 0) {
-        app.innerHTML = "<p>No data available.</p>";
-        return;
+
+function displayData() {
+    let content = '<ul>';
+
+    if (axiosData.length === 0) {
+        content += `<p>No Axios Data Found</p>`;
+    }  
+     else {
+        content += "<h3>Axios Data (Users)</h3>";
+        axiosData.forEach(user => {
+            content += `<li>${user.name} - ${user.email}</li>`;
+        });
+            content += "</ul>";
     }
 
-    //very unsure as to how i should print out the data the way i want, this comment will disapear when i figure it out.
-    app.innerHTML = /*HTML*/`
-    <ul>
-       
-    </ul>
-    `;
+    
+    if (fetchData.length === 0) {
+        content += `<p>No Fetch Data Found</p>`;
+    }   
+    else {
+        content += "<h3>Fetch Data (Posts)</h3><ul>";
+        fetchData.forEach(post => {
+            content += `<li>${post.title} - ${post.body}</li>`;
+        });
+            content += "</ul>";
+    }
+
+    
+    app.innerHTML = content;
 }
-
-      
-    
-    
-     
-
-    
-
-
